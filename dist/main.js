16545410783ea3d236344c6a73409d54dcd11ad2
bdaneels <brecht.daneels@hotmail.com>
2023-05-08 17:43:23 +0200

    add some initial objects

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\n\n\n//# sourceURL=webpack://javascript-starter/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst pageLoad = (() => {\n    function onLoad (){\n        let form = document.querySelector('form')\n        form.addEventListener('submit', (event)=>{\n            if(true){\n              event.preventDefault()\n              showError()\n            } else{}\n          })\n    }\n    return{\n        onLoad\n    }\n } )()\n\n const errorHandler = (() => {\n    function showError(){\n       \n    }\n    return{\n        showError\n    }\n } )()\n\n const validityHandler = (() => {\n    function valid (){\n        let email = document.getElementById('email')\n        let zip = document.getElementById('zip')\n        let country = document.getElementById('country')\n        let password = document.getElementById('password')\n        let password2 = document.getElementById('passwordc')\n\n        \n    }\n    return{\n        \n    }\n } )()\n\npageLoad.onLoad()\n\n//# sourceURL=webpack://javascript-starter/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 import './style.css'
 
+const pageLoad = (() => {
+    function onLoad (){
+        let form = document.querySelector('form')
+        form.addEventListener('submit', (event)=>{
+            if(true){
+              event.preventDefault()
+              showError()
+            } else{ submitClick(event)
+          
+            }
+          })
+    }
+    return{
+        onLoad
+    }
+ } )()
+
+ const errorHandler = (() => {
+    function showError(){
+       
+    }
+    return{
+        showError
+    }
+ } )()
+
+ const validityHandler = (() => {
+    function valid (){
+        let email = document.getElementById('email')
+        let zip = document.getElementById('zip')
+        let country = document.getElementById('country')
+        let password = document.getElementById('password')
+        let password2 = document.getElementById('passwordc')
+
+        
+    }
+    return{
+        
+    }
+ } )()
+
+pageLoad.onLoad()
