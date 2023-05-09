a30f033d82ad3f2f4e17f156f0fb9b8650d5aa96
bdaneels <brecht.daneels@hotmail.com>
2023-05-09 11:41:29 +0200

    work on eventlisteners

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import './style.css'
 const pageLoad = (() => {
     function onLoad (){
         let form = document.querySelector('form')
-        form.addEventListener('submit', (event)=>{
-            if(true){
+        form.addEventListener('change', (event)=>{
+            if(!validityHandler.valid(event)){
               event.preventDefault()
-              showError()
-            } else{ submitClick(event)
+              errorHandler.showError(event)
+            } else{ 
+                errorHandler.clearError(event)
           
             }
           })
+        form.addEventListener('submit', (event)=>{
+            if(true){
+              event.preventDefault()
+              
+            } else{ 
+                errorHandler.clearError(event)
+          
+            }
+          })  
     }
     return{
         onLoad
@@ -18,26 +28,49 @@ const pageLoad = (() => {
  } )()
 
  const errorHandler = (() => {
-    function showError(){
+    function showError(event){
+        console.log('showerror called')
+       let element = document.getElementById(event.target.id)
+       let span = element.nextSibling
+       let data = span.getAttribute('data')
        
+       if(element.validity.valueMissing || !element.validity.valid){
+        span.textContent = `Please enter a valid ${data}`
+       }
+
+    }
+
+    function clearError(event){
+        let element = document.getElementById(event.target.id)
+        let span = element.nextSibling
+        span.textContent = ''
     }
     return{
-        showError
+        showError,
+        clearError
     }
  } )()
 
  const validityHandler = (() => {
-    function valid (){
-        let email = document.getElementById('email')
+    function valid (event){
+        
+        /* let email = document.getElementById('email')
         let zip = document.getElementById('zip')
         let country = document.getElementById('country')
         let password = document.getElementById('password')
-        let password2 = document.getElementById('passwordc')
+        let password2 = document.getElementById('passwordc') */
+
+        let element = document.getElementById(event.target.id)
+
+        if(!element.validity.valid || element.validity.valueMissing){
+            console.log(`invalid ${element}`)
+            return false
+        } else {return true}
 
         
     }
     return{
-        
+        valid
     }
  } )()
 
