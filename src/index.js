deabd142640d88a667fc529baeb164e08c5c267c
bdaneels <brecht.daneels@hotmail.com>
2023-05-10 14:06:55 +0200

    fix formatting with prettier

--- src/index.js
+++ src/index.js
@@ -1,140 +1,132 @@
-import './style.css'
+import "./style.css";
 
 const pageLoad = (() => {
-    function onLoad (){
-        let form = document.querySelector('form')
-        form.addEventListener('change', (event)=>{
-            if(!validityHandler.valid(event)){
-              event.preventDefault()
-              let element = document.getElementById(event.target.id)
-              errorHandler.showError(element)
-            } else{ 
-                errorHandler.clearError(event)
-          
-            }
-          })
-        form.addEventListener('submit', (event)=>{
-            if(!validityHandler.formCheckValid()){
-              event.preventDefault()
-              
-              
-            } else{ 
-                event.preventDefault()
-                console.log('high Five')
-          
-            }
-          })  
-    }
-    return{
-        onLoad
-    }
- } )()
+  function onLoad() {
+    let form = document.querySelector("form");
+    form.addEventListener("change", (event) => {
+      if (!validityHandler.valid(event)) {
+        event.preventDefault();
+        let element = document.getElementById(event.target.id);
+        errorHandler.showError(element);
+      } else {
+        errorHandler.clearError(event);
+      }
+    });
+    form.addEventListener("submit", (event) => {
+      if (!validityHandler.formCheckValid()) {
+        event.preventDefault();
+      } else {
+        event.preventDefault();
+        console.log("high Five");
+      }
+    });
+  }
+  return {
+    onLoad,
+  };
+})();
 
- const errorHandler = (() => {
-    function showError(element){
-        console.log('showerror called')
-       
-       
-       if(element.id==="password"|| element.id==="passwordc"){
-            let password = document.getElementById('password')
-            let password2 = document.getElementById('passwordc')
-            let errorSpan = document.getElementById('passworderror')
-            if ( password2.value !== password.value){
-                errorSpan.textContent = 'Passwords do not match'
-                element.classList.add('invalid')}
-                else if (password.validity.valueMissing|| password2.validty.valueMissing){
-                errorSpan.textContent = 'Please enter a password'
-                element.classList.add('invalid')
-                }
-            
-            }
-        else if(element.validity.valueMissing || !element.validity.valid){
-            let span = element.nextSibling
-            let data = span.getAttribute('data')
-            span.textContent = `Please enter a valid ${data}`
-            element.classList.add('invalid')
+const errorHandler = (() => {
+  function showError(element) {
+    console.log("showerror called");
+
+    if (element.id === "password" || element.id === "passwordc") {
+      let password = document.getElementById("password");
+      let password2 = document.getElementById("passwordc");
+      let errorSpan = document.getElementById("passworderror");
+      if (password2.value !== password.value) {
+        errorSpan.textContent = "Passwords do not match";
+        element.classList.add("invalid");
+      } else if (
+        password.validity.valueMissing ||
+        password2.validty.valueMissing
+      ) {
+        errorSpan.textContent = "Please enter a password";
+        element.classList.add("invalid");
+      }
+    } else if (element.validity.valueMissing || !element.validity.valid) {
+      let span = element.nextSibling;
+      let data = span.getAttribute("data");
+      span.textContent = `Please enter a valid ${data}`;
+      element.classList.add("invalid");
+    }
+  }
+
+  function clearError(event) {
+    let element = document.getElementById(event.target.id);
+    console.log(element);
+
+    if (element.id === "password" || element.id === "passwordc") {
+      let errorSpan = document.getElementById("passworderror");
+      errorSpan.textContent = "";
+      let password = document.getElementById("password");
+      let password2 = document.getElementById("passwordc");
+      password.classList.remove("invalid");
+      password2.classList.remove("invalid");
+    } else {
+      let span = element.nextSibling;
+      span.textContent = "";
+      element.classList.remove("invalid");
+    }
+  }
+  return {
+    showError,
+    clearError,
+  };
+})();
+
+const validityHandler = (() => {
+  function valid(event) {
+    let password = document.getElementById("password");
+    let password2 = document.getElementById("passwordc");
+
+    let element = document.getElementById(event.target.id);
+
+    if (element.id === "password" || element.id === "passwordc") {
+      if (password2.value !== password.value) {
+        console.log("passwords do not match");
+        return false;
+      } else {
+        return true;
+      }
+    } else if (!element.validity.valid || element.validity.valueMissing) {
+      console.log(`invalid ${element.id}`);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function formCheckValid() {
+    let inputs = document.querySelectorAll("input");
+    let password = document.getElementById("password");
+    let password2 = document.getElementById("passwordc");
+    for (let element of inputs) {
+      
+      if (element.id === "password" || element.id === "passwordc") {
+        if (password2.value === "" || password.value === "") {
+          errorHandler.showError(element);
+          return false;
+        } else if (password2.value !== password.value) {
+          console.log("passwords do not match");
+          return false;
         }
-
-        }
-    
-
-    function clearError(event){
-        let element = document.getElementById(event.target.id)
-        console.log(element)
-        
-        if(element.id === "password" || element.id === 'passwordc'){
-            let errorSpan = document.getElementById("passworderror")
-            errorSpan.textContent = ''
-            let password = document.getElementById('password')
-            let password2 = document.getElementById('passwordc')
-            password.classList.remove('invalid')
-            password2.classList.remove('invalid')
-        }else {
-            let span = element.nextSibling
-            span.textContent = ''
-            element.classList.remove('invalid')
-        }
-        
+      } else if (
+        !element.validity.valid ||
+        element.validity.valueMissing ||
+        element.value === ""
+      ) {
+        errorHandler.showError(element);
+        return false;
+      }
     }
-    return{
-        showError,
-        clearError
-    }
-    } )()
+    return true;
+  }
 
- const validityHandler = (() => {
-    function valid (event){
-        
-        let password = document.getElementById('password')
-        let password2 = document.getElementById('passwordc')
+  return {
+    valid,
+    formCheckValid,
+  };
+})();
 
-        let element = document.getElementById(event.target.id)
-
-        if(element.id === 'password' || element.id === 'passwordc'){
-            
-            if ( password2.value !== password.value){
-                console.log('passwords do not match')
-                return false
-            } else {return true}
-        }
-        else if(!element.validity.valid || element.validity.valueMissing){
-            console.log(`invalid ${element.id}`)
-            return false
-        } else {return true}
-
-        
-        
-    }
-
-    function formCheckValid(){
-        let inputs = document.querySelectorAll('input')
-        let password = document.getElementById('password')
-        let password2 = document.getElementById('passwordc')
-        for(let element of inputs){
-            console.log(element)
-            if(element.id === 'password' || element.id === 'passwordc'){
-                
-                if( password2.value === '' || password.value === '' ){
-                    errorHandler.showError(element)
-                    return false
-                }
-                else if( password2.value !== password.value){
-                    console.log('passwords do not match')
-                    return false
-                }
-            }
-            else if(!element.validity.valid || element.validity.valueMissing || element.value === ''){
-                errorHandler.showError(element)
-                return false
-            } 
-        }
-        return true
-    }
-
-    return{
-        valid,
-        formCheckValid
-    }
- } )()
-
-pageLoad.onLoad()
+pageLoad.onLoad();
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js',
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
-      port: 3000,
-      open: true,
-      hot: true,
-      compress: true,
-      historyApiFallback: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          include: path.resolve(__dirname, 'src'),
-          use: ['style-loader', 'css-loader', 'postcss-loader'],
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
-      ],
-    },
-  }
+      },
+    ],
+  },
+};
